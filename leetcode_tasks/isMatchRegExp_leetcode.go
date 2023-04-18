67aea65b11b017251656ef36a85d9ed22d30fe8f
Semyon Bolonin <125894814+SAMFATEWOLF@users.noreply.github.com>
2023-04-18 18:42:14 +0300

    Add files via upload

---
+++ leetcode_tasks/isMatchRegExp_leetcode.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+// '.' Matches any single character.​​​​
+// '*' Matches zero or more of the preceding element.
+// The matching should cover the entire input string (not partial).
+
+func isMatch(s string, p string) bool {
+	res, err := regexp.Compile(fmt.Sprintf("^%s$", p))
+	if err != nil {
+		panic(err)
+	}
+	resBool := res.Match([]byte(s))
+	return resBool
+
+}
+
+func main() {
+	s := "aa"
+	p := ".*"
+	fmt.Println(isMatch(s, p))
+}
